1b1d07e6ed34406bcd992aaeed2691dd651a7a7d
Maud Boucherit <maud.boucherit@gmail.com>
2018-01-27 13:48:55 -0800

    Change column names

--- src/EDA.py
+++ src/EDA.py
@@ -12,7 +12,7 @@ def missing_values_table(df):
         mis_val_percent = 100 * df.isnull().sum() / len(df)
         mis_val_table = pd.concat([mis_val, mis_val_percent], axis=1)
         mis_val_table_ren_columns = mis_val_table.rename(
-        columns = {0 : 'Missing Values', 1 : '% of Total Values'})
+        columns = {0 : 'nb_na', 1 : 'pct_na'})
         return mis_val_table_ren_columns 
         
 na_train = missing_values_table(train)
